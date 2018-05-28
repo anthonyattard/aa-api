2fe86dd70060f012b17652cbe7bed1cfb42cf3f6
Anthony Attard <anthony@anthonyattard.com>
2018-05-27 23:35:58 -0700

    Add routes/index.js require into server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const app        = express();
 
 const port = 8000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+require('./app/routes')(app);
+
 app.listen(port, () => {
   console.log("We are live on " + port);
 })
