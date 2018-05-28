02e4f5aadb6989a71e12141023b8036311f0ba7a
Anthony Attard <anthony@anthonyattard.com>
2018-05-28 00:28:04 -0700

    Integrated request-promise to get spitcast data

--- app/routes/surf_routes.js
+++ app/routes/surf_routes.js
@@ -1,10 +1,34 @@
 module.exports = function(app) {
+  var rp = require('request-promise');
+
   app.get('/surf', (req, res) => {
     const lat = req.query.lat;
     const long = req.query.long;
     const dist = req.query.dist;
 
-    var spit_cast_url = `http://api.spitcast.com/api/spot-forecast/search?latitude=${lat}&longitude=${long}&distance=${dist}`;
+    var spit_cast_url = 'http://api.spitcast.com/api/spot-forecast/search';
+
+    var options = {
+        uri: spit_cast_url,
+        qs: {
+            latitude: lat, // -> uri + '?access_token=xxxxx%20xxxxx'
+            longitude: long,
+            distance: dist
+        },
+        headers: {
+            'User-Agent': 'Request-Promise'
+        },
+        json: true // Automatically parses the JSON string in the response
+    };
+
+    rp(options)
+        .then(function (data) {
+            console.log(data);
+            res.send(data);
+        })
+        .catch(function (err) {
+            // API call failed...
+        });
 
   });
 }
