60c122285e03a3299da3ac4a3dbda0cb93a802b2
Anthony Attard <anthony@anthonyattard.com>
2018-05-28 02:39:39 -0700

    Allowed CORS from surf.anthonyattard.com

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const app        = express();
 
 const port = 8080;
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://surf.anthonyattard.com");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 require('./app/routes')(app);
