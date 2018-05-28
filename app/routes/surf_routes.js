02732a2a1352a6be2ade85e9bff495c83ac7a688
Anthony Attard <anthony@anthonyattard.com>
2018-05-28 00:19:48 -0700

    Removed .ajax call

--- app/routes/surf_routes.js
+++ app/routes/surf_routes.js
@@ -6,14 +6,5 @@ module.exports = function(app) {
 
     var spit_cast_url = `http://api.spitcast.com/api/spot-forecast/search?latitude=${lat}&longitude=${long}&distance=${dist}`;
 
-   $.ajax({
-      url: spit_cast_url,
-      dataType: 'json',
-      data: data,
-      success: function() {
-        res.send("Hey you!");
-      }
-    });
-
   });
 }
